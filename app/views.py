2acbbbc8ca67eb0ebac00c73dc8494e7932a53dc
GloriaTT502a <144274771+GloriaTT502a@users.noreply.github.com>
2024-10-23 21:13:39 -0700

    Update views.py

--- app/views.py
+++ app/views.py
@@ -143,6 +143,9 @@ def emergency_access(request):
         access.save()
         return JsonResponse({'access_id':access.access_id, 'status':'success'})
 
+########################################
+####Defined by Gloria on Oct 07, 2024###
+########################################
 def emergency_access_grant(request):
     if request.method != 'POST':
         return JsonResponse({'status':'post required'})
